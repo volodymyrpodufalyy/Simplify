6735b51bac9ada725de3bcce0eed4e723d75c663
volodymyrpodufalyy <vpodufalyy13@gmail.com>
2023-02-01 11:09:21 +0200

    Remove some boilerplate code

--- app/utils/storage/storage.ts
+++ app/utils/storage/storage.ts
@@ -1,35 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
 /**
- * Loads a string from storage.
- *
- * @param key The key to fetch.
- */
-export async function loadString(key: string): Promise<string | null> {
-  try {
-    return await AsyncStorage.getItem(key)
-  } catch {
-    // not sure why this would fail... even reading the RN docs I'm unclear
-    return null
-  }
-}
-
-/**
- * Saves a string to storage.
- *
- * @param key The key to fetch.
- * @param value The value to store.
- */
-export async function saveString(key: string, value: string): Promise<boolean> {
-  try {
-    await AsyncStorage.setItem(key, value)
-    return true
-  } catch {
-    return false
-  }
-}
-
-/**
  * Loads something from storage and runs it thru JSON.parse.
  *
  * @param key The key to fetch.
